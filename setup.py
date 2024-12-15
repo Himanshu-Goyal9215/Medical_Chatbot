ff4b93db75eb75bc20f9aa3b570b22a39577fb95
Himanshu <goyalhimanshu096@gmail.com>
2024-12-15 19:09:27 +0530

    commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Himanshu Goyal',
+    author_email= 'goalhimanshu096@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
